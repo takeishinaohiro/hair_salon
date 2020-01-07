1e9413b8439dd8edc577232d670f6e399eff1985
tikayk <tikayk@gmail.com>
2020-01-07 18:10:12 +0900

    トップページ編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_31_080844) do
+ActiveRecord::Schema.define(version: 2019_12_31_080832) do
 
   create_table "hairs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image1"
